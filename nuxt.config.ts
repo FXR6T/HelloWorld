1d1d2345e6267a681330540c348d995ddfb5c4cc
FXR6T <bchai@ezytouch.com>
2024-10-19 18:19:45 +1100

    nbt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       // Can be overridden by NUXT_PUBLIC_HELLO_TEXT environment variable
-      helloText: '...from Adele - Hello 👋',
+      helloText: process.env.NUXT_PUBLIC_HELLO_TEXT,
       apiSecret: '123'
     }
   },
